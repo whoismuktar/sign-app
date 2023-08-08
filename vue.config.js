8fe4a933c9401aae04bdf15225c4eef761f719d5
Muktar Yusuf <mayunited12@yahoo.com>
2023-08-08 08:35:40 +0100

    install, setup needed packages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require("@vue/cli-service")
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: ["vuetify"],
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/_variables.scss";`,
+      },
+    },
+  },
+});
