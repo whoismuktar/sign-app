733e394a833f1f775f583e47fe7e97a6871e296e
Muktar Yusuf <mayunited12@yahoo.com>
2023-08-10 00:12:39 +0100

    doc card modal progress

--- src/components/DocCard.vue
+++ src/components/DocCard.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="doc-card">
-    <v-icon aria-hidden="false"> mdi-file </v-icon>
+  <div class="doc-card-wrapper">
+    <div class="doc-card" @click="viewDoc = true">
+      <v-icon aria-hidden="false"> mdi-file </v-icon>
+    </div>
+
+    <v-dialog
+      v-model="viewDoc"
+      transition="slide-x-reverse-transition"
+      fullscreen
+      content-class="doc-card-modal"
+      overlay-color="#000000"
+    >
+      <!-- overlay-color="rgba(0,0,0,0.5)" -->
+      <div class="modal-decoy" @click="closeDoc"></div>
+      <div class="doc-preview">HELLO</div>
+    </v-dialog>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      viewDoc: false,
+    };
+  },
+  methods: {
+    closeDoc() {
+      this.viewDoc = false
+    },
+  },
   props: {
     document: Object,
   },
@@ -13,14 +37,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.doc-card {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 70px 50px;
-  margin-bottom: 10px;
-  border-radius: 20px;
-  cursor: pointer;
-  background-color: rgba($color: $secondary-color, $alpha: 1.0);
+.doc-card-wrapper {
+  .doc-card {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 70px 50px;
+    margin-bottom: 10px;
+    border-radius: 20px;
+    cursor: pointer;
+    background-color: rgba($color: $secondary-color, $alpha: 1);
+  }
+}
+.doc-preview {
+  background-color: #ffffff;
+  height: 100vh;
+  width: 50%;
+  position: absolute;
+  right: 0;
+  padding: 40px;
 }
 </style>
